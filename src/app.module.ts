0cf4780e6e4c391870dc168599d32cdbb3def1fe
numchew <dekchainumchew@gmail.com>
2023-12-01 17:33:14 +0700

    fix bug env

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,15 +7,26 @@ import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { ReportApp1Module } from './report_app1/report_app1.module';
 
-import * as path from 'path';
+//import * as path from 'path';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    TypeOrmModule.forRootAsync({
+    /* TypeOrmModule.forRootAsync({
       useFactory: () => ({
         ...require(path.resolve(__dirname, 'ormconfig.js')),
       }),
+    }), */
+
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      autoLoadEntities: true,
+      synchronize: true,
     }),
     UsersModule,
     AuthModule,
